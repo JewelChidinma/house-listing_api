3038715b88ef3d0247679759bd66df30bae423f8
Chidinma Osonwa-Paul <jewelchidinma@gmail.com>
2025-09-16 18:44:49 +0100

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/house_listings';
+    const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:3000/house_listings';
     
     await mongoose.connect(mongoURI);
     console.log('MongoDB connected successfully');
